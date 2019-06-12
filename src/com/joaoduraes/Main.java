9c75ceb2b7a0f094e9a9d277b56393bd29b3621b
joaoduraes <joaoduraes@gmail.com>
2019-06-12 21:47:54 +0100

    birthdayCakeCandles and timeConversion

--- src/com/joaoduraes/Main.java
+++ src/com/joaoduraes/Main.java
@@ -41,14 +41,24 @@ public class Main {
 
         //staircase(101);
 
-        int[] arr = new int[5];
+        /*int[] arr = new int[5];
         arr[0] = 1;
         arr[1] = 2;
         arr[2] = 3;
         arr[3] = 4;
         arr[4] = 5;
 
-        miniMaxSum(arr);
+        miniMaxSum(arr);*/
+/*
+        int[] arr = new int[4];
+        arr[0] = 3;
+        arr[1] = 2;
+        arr[2] = 1;
+        arr[3] = 3;
+
+        birthdayCakeCandles(arr);*/
+
+        timeConversion("12:05:45PM");
     }
 
     /*
@@ -220,5 +230,77 @@ public class Main {
         System.out.println(mini + " " + max);
     }
 
+    // Complete the birthdayCakeCandles function below.
+    static int birthdayCakeCandles(int[] ar) {
+
+        double tenFive = Math.pow(10, 5);
+        double tenSeven = Math.pow(10, 7);
+        if (ar == null || ar.length == 0 || ar.length > tenFive) {
+            throw new IllegalArgumentException();
+        }
+
+
+        List<Integer> candles = new ArrayList<>();
+
+        for (int i : ar) {
+            if (i < 1 || i > tenSeven) {
+                throw new IllegalArgumentException();
+            }
+            candles.add(i);
+        }
+
+        candles.sort(Integer::compareTo);
+
+        Integer maxHeight = candles.get(candles.size() - 1);
+
+        int numOfBlownCandles = 0;
+        for (Integer candle : candles) {
+            if (candle.equals(maxHeight)) {
+                numOfBlownCandles++;
+            }
+        }
+
+
+        return numOfBlownCandles;
+
+    }
+
+    /*
+     * Complete the timeConversion function below.
+     */
+    static String timeConversion(String s) {
+        String[] split = s.split(":");
+
+        try {
+            String hh = split[0];
+            String mm = split[1];
+            String ssXM = split[2];
+            String ss = ssXM.substring(0, 2);
+            String XM = ssXM.substring(2, 4);
+
+            int hhInt = Integer.parseInt(hh);
+            int mmInt = Integer.parseInt(mm);
+            int ssInt = Integer.parseInt(ss);
+
+            if (hhInt < 0 || hhInt > 12
+                    || mmInt < 0 || mmInt > 59
+                    || ssInt < 0 || ssInt > 59
+                    || (!"AM".equals(XM) && !"PM".equals(XM))) {
+                throw new IllegalArgumentException();
+            }
+
+            if ("PM".equals(XM)) {
+                if (hhInt != 12) {
+                    hh = String.valueOf(hhInt + 12);
+                }
+            } else if (hhInt == 12) {
+                hh = "00";
+            }
+
+            return hh + ":" + mm + ":" + ss;
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+    }
 
 }
